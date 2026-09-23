fb395a7b9adab180e9ef575c86a6bd5fdd4ebdf1
yannickfan67-ai <yannickfan67@gmail.com>
2026-09-24 01:08:30 +0800

    test: add JVM JSON parser dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,5 +42,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
-    testImplementation("junit:junit:4.13.2")
+    testImplementation("junit:junit:4.13.2")\n    testImplementation("org.json:json:20240303")
 }
