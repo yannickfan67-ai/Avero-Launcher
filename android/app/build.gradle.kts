52b008ee5dcf711ddd7b7c7de692603e092030f5
yannickfan67-ai <yannickfan67@gmail.com>
2026-09-24 01:05:12 +0800

    test: add launcher rule unit test dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,4 +42,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
+    testImplementation("junit:junit:4.13.2")
 }
