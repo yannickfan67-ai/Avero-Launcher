a74e6da21043408045c5b92abed228a96300a365
yannickfan67-ai <yannickfan67@gmail.com>
2026-09-24 00:50:52 +0800

    feat: add InstanceLayout.kt

---
+++ android/app/src/main/java/io/yannickfan/avero/minecraft/InstanceLayout.kt
@@ -0,0 +1,23 @@
+package io.yannickfan.avero.minecraft
+
+import java.io.File
+
+class InstanceLayout(private val root: File) {
+    fun versionDirectory(versionId: String) = File(root, "versions/$versionId")
+    fun versionJson(versionId: String) = File(versionDirectory(versionId), "$versionId.json")
+    fun clientJar(versionId: String) = File(versionDirectory(versionId), "$versionId.jar")
+
+    fun library(path: String) = File(root, "libraries/$path")
+
+    fun assetIndex(id: String) = File(root, "assets/indexes/$id.json")
+    fun assetObject(hash: String): File {
+        require(hash.length >= 2)
+        return File(root, "assets/objects/${hash.take(2)}/$hash")
+    }
+
+    fun gameDirectory(instanceName: String) =
+        File(root, "instances/${sanitize(instanceName)}/game")
+
+    private fun sanitize(value: String): String =
+        value.replace(Regex("[^A-Za-z0-9._-]"), "_")
+}
