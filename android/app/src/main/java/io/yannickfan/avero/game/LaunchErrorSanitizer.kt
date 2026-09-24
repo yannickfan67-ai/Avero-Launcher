6c34c21c114b2ac37441e47f16c40108e6b11092
yannickfan67-ai <yannickfan67@gmail.com>
2026-09-24 18:00:59 +0800

    fix: normalize launch error redaction labels

--- android/app/src/main/java/io/yannickfan/avero/game/LaunchErrorSanitizer.kt
+++ android/app/src/main/java/io/yannickfan/avero/game/LaunchErrorSanitizer.kt
@@ -6,8 +6,20 @@ internal fun sanitizeLaunchError(t: Throwable): String {
 
     return raw
         .replace(
-            Regex("(?i)(access[_ -]?token|refresh[_ -]?token|client[_ -]?secret|authorization|bearer)\\s*[:=]\\s*\\S+"),
-            "$1=<redacted>"
+            Regex("(?i)access[_ -]?token\\s*[:=]\\s*\\S+"),
+            "accessToken=<redacted>"
+        )
+        .replace(
+            Regex("(?i)refresh[_ -]?token\\s*[:=]\\s*\\S+"),
+            "refreshToken=<redacted>"
+        )
+        .replace(
+            Regex("(?i)client[_ -]?secret\\s*[:=]\\s*\\S+"),
+            "clientSecret=<redacted>"
+        )
+        .replace(
+            Regex("(?i)authorization\\s*[:=]\\s*\\S+"),
+            "Authorization=<redacted>"
         )
         .replace(
             Regex("(?i)bearer\\s+\\S+"),
